aabef891f7954f53e6f0137ddb32b8da061e7430
nohira hiroki <nohira.h.aa@m.titech.ac.jp>
2019-06-20 22:41:05 +0900

    Implementing VkPipelineLayout.

--- src/vulkan/shader/pipeline.rs
+++ src/vulkan/shader/pipeline.rs
@@ -1,5 +1,10 @@
-mod stage;
-
+pub mod stage;
 
 use super::*;
+
+pub struct VkPipelineLayout<L> {
+	device: Arc<VkDevice>,
+	handle: vk::PipelineLayout,
+	descriptor_layouts: L,
+}
 
---
+++ src/vulkan/shader/pipeline/stage.rs
@@ -0,0 +1,50 @@
+use ash::vk::PipelineStageFlags;
+
+pub trait PipelineStages {
+	fn pipeline_stages() -> PipelineStageFlags;
+}
+
+pub struct Empty;
+impl PipelineStages for Empty {
+	fn pipeline_stages() -> PipelineStageFlags { PipelineStageFlags::empty() }
+}
+
+macro_rules! impl_pipeline_stage {
+	(($name: ident, $flag: ident,)*) => {$(
+		pub struct $name<S>(pub S) where PipelineStages;
+		impl<S> PipelineStages for $name<S> where S: PipelineStages {
+			fn pipeline_stages() -> PipelineStageFlags {
+				PipelineStageFlags::$flag | S::pipeline_stages()
+			}
+		}
+	)*};
+}
+
+impl_pipeline_stage!(
+	TopOfPipe, TOP_OF_PIPE
+	DrawIndirect, DRAW_INDIRECT
+	VertexInput, VERTEX_INPUT
+	VertexShader, VERTEX_SHADER
+	TessellationControlShader, TESSELLATION_CONTROL_SHADER
+	TessellationEvaluationShader, TESSELLATION_EVALUATION_SHADER
+	GeometryShader, GEOMETRY_SHADER
+	FragmentShader, FRAGMENT_SHADER
+	EarlyFragmentTests, EARLY_FRAGMENT_TESTS
+	lateFragmentTests, LATE_FRAGMENT_TESTS
+	ColorAttachmentOutput, COLOR_ATTACHMENT_OUTPUT
+	ComputeShader, COMPUTE_SHADER
+	Transfer, TRANSFER
+	BottomOfPipe, BOTTOM_OF_PIPE
+	Host, HOST
+	AllGraphics, ALL_GRAPHICS
+	AllCommands, ALL_COMMANDS
+	TransformFeedbackExt, TRANSFORM_FEEDBACK_EXT
+	ConditionalRenderingExt, CONDITIONAL_RENDERING_EXT
+	ComandProcessNvx, COMMAND_PROCESS_NVX
+	ShadingRateImageNv, SHADING_RATE_IMAGE_NV
+	RayTracingShaderNv, RAY_TRACING_SHADER_NV
+	AccelerationStructbuildNv, ACCELERATION_STRUCTBUILD_NV
+	TaskShaderNv, TASK_SHADER_NV
+	MeshShaderNV, MESH_SHADER_NV
+	FragmentDensityProdessExt, FRAGMENT_DENSITY_PROCESS_EXT
+);
